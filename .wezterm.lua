6f343d5d3b75c8845678183c38e78f6cb0a1d81c
Bradley Taylor <qzi@qlik.com>
2025-05-26 23:59:28 -0700

    update wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font_size = 18
+config.color_scheme = "Tokyo Night"
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+-- cursor styles
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.cursor_blink_rate = 500
+
+return config
